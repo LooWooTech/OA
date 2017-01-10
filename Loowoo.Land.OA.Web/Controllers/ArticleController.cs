d6a4c155b55b6739f7880f23487aa3d9436fa7e5
Liangjun Zheng <zhengliangjun@outlook.com>
2017-01-10 16:09:47 +0800

    添加信息列表

---
+++ Loowoo.Land.OA.Web/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using Loowoo.Land.OA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loowoo.Land.OA.Web.Controllers
+{
+    public class ArticleController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id = 0)
+        {
+            return View();
+        }
+
+        public void Save(Article model)
+        {
+
+        }
+    }
+}
---
+++ Loowoo.Land.OA/Models/Article.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loowoo.Land.OA.Models
+{
+    public class Article
+    {
+        public Article()
+        {
+            CreateTime = DateTime.Now;
+        }
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public string Description { get; set; }
+
+        public User Creator { get; set; }
+
+        public int Views { get; set; }
+
+        public string DefaultImage { get; set; }
+
+        public bool Checked { get; set; }
+
+        public bool Deleted { get; set; }
+
+        public string Content { get; set; }
+    }
+}
